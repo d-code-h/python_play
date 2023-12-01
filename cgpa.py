82dd7d946fe524372548aced0532b6b8f6c13789
Habeeb Yunus Habeeb <habeebdh1@outlook.com>
2023-12-01 17:41:54 +0100

    Collect first semester course details

--- cgpa.py
+++ cgpa.py
@@ -11,12 +11,15 @@ def courseDetails():
   unit = input("Course unit in numbers: ")
   score = input("Score (Total score over 100 in numbers): ")
 
-  return {
-    name, unit, score
-  }
+  return [name, unit, score]
 
 def start ():
   semester = input('Enter your choice semester. \n\n1 = First Semester \n2 = Second Semester\n')
+
+  student = {
+    "1": [],
+    "2": []
+  }
 
   try:
     semester = int(semester)
@@ -28,6 +31,18 @@ def start ():
         print("Details of course " + str(i))
         course = courseDetails()
         print(course)
+        try:
+          course[1] = int(course[1])
+          course[2] = int(course[2])
+          index = str(semester)
+          student[index].append(course)
+
+          print(index)
+          print(student)
+        except:
+          print("Wrong! You didn't enter a number either on unit or score")
+          course = courseDetails()
+
 
     elif a == 2:
       print("Wow! You are almost leaving")
