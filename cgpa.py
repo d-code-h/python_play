95da998447c3c705e7750b25f0764de19023c2fc
Habeeb Yunus Habeeb <habeebdh1@outlook.com>
2023-12-01 18:53:23 +0100

    collect course details for semester 1 and 2

--- cgpa.py
+++ cgpa.py
@@ -6,6 +6,12 @@
 # Welcome message
 print("Welcome to CGPA Calculator\n")
 
+student = {
+    "1": [],
+    "2": []
+  }
+
+
 def courseDetails():
   name = input("Enter course name: ")
   unit = input("Course unit in numbers: ")
@@ -14,42 +20,47 @@ def courseDetails():
   return [name, unit, score]
 
 def start ():
+  print(student)
   semester = input('Enter your choice semester. \n\n1 = First Semester \n2 = Second Semester\n')
 
-  student = {
-    "1": [],
-    "2": []
-  }
-
+  print(len(student[semester]))
   try:
-    semester = int(semester)
-    if semester == 1:
+    # semester = int(semester)
+    print()
+    if len(student[semester]) == 0:
+   # if semester == 1:
       courseCount = int(input("How many courses did you offer? (Respond with number): "))
       
-      for i in range(1,courseCount):
+      for i in range(1,courseCount+1):
         # Get course details
+        print("\n=====================")
         print("Details of course " + str(i))
+        print("=====================")
         course = courseDetails()
-        print(course)
+        #print(course)
         try:
           course[1] = int(course[1])
           course[2] = int(course[2])
           index = str(semester)
           student[index].append(course)
 
-          print(index)
-          print(student)
         except:
           print("Wrong! You didn't enter a number either on unit or score")
           course = courseDetails()
-
-
-    elif a == 2:
-      print("Wow! You are almost leaving")
+      print("\n=====================")
+        
+      print("Done with semester " + semester + "")
+      print("=====================\n") 
+      
+    #elif a == 2:
+    elif len(student[semester]) > 0:
+        print("Sorry, you have added the courses for this semester. Enter the courses for the other semester now. ")
+      #print("Wow! You are almost leaving")
     else:
       print("): You entered the wrong number. Try again")
   except:
     print("Wrong! You didn't enter a number")
     start()
+  start()
 
 start()
